4a37c0defb8130c2e5cb9a690be037ad09e43a5c
Tony Spaskyi <jim.casey321@gmail.com>
2017-12-23 20:54:13 +0200

    Try creating db instead of updating it

--- exampleapp/settings.py
+++ exampleapp/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = { 'default': dj_database_url.config(conn_max_age=500) }
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
